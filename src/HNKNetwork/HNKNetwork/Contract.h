5736343aabcf22eada28f9ff405d13f88610cff6
Erika Chiba <19531669+InoriTakenaka@users.noreply.github.com>
2026-03-20 16:30:13 +0800

    create contract

---
+++ src/HNKNetwork/HNKNetwork/Contract.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include<cstddef>
+#include<cstdint>
+
+namespace HNKNetwork
+{
+	enum class ContractType : std::uint8_t
+	{
+		None = 0,
+		Handshake = 1,
+		Data = 2,
+		Termination = 3
+	};
+#pragma pack(push, 1)
+	struct Contract
+	{
+	public:
+		const std::byte ContractHeader{ 0x0F };
+		int				LengthIndicator;
+		ContractType 	Command;
+		std::byte		Payload[0];
+	};
+#pragma pack(pop)
+}
